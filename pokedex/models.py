21abc41f53aa2e6691bfcb657014e7664545a0e9
cero96 <m.antoniocero96@hotmail.es>
2024-07-13 07:37:53 -0500

    laboratorio8

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 # Create your models here.
+# create pokemon from model set
+ 
 class Pokemon(models.Model):
-    name=models.CharField(max_length=30,null=False)
-    type=models.CharField(max_length=30,null=False)
-    weight=models.IntegerField(null=False,default=1)
-    height=models.IntegerField(null=False,default=1)
+    name = models.CharField(max_length=30, null=False)
+    POKEMON_TYPES = {
+        ("A", "Agua"),
+        ("F", "Fuego"),
+        ("T", "Tierra"),
+        ("P", "Planta"),
+        ("E", "Eléctrico")
+    }
+    type = models.CharField(max_length=30, choices=POKEMON_TYPES, null=False)
+    weight = models.DecimalField(null=False, default=1, max_digits=4, decimal_places=2)
+    height = models.DecimalField(null=False, default=1, max_digits=4, decimal_places=2)
+    
     
     def __str__(self) -> str:
         return self.name
